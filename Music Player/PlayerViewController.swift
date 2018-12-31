fb09258e6e4877f5e7f54ac39612231d191d5f2b
Ben Todd <btodd@multiservice.com>
2018-12-31 15:13:18 -0600

    Added login view

--- Music Player/PlayerViewController.swift
+++ Music Player/PlayerViewController.swift
@@ -73,6 +73,9 @@ class PlayerViewController: UIViewController, UITableViewDelegate,UITableViewDat
     @IBOutlet var blurImageView : UIImageView!
     @IBOutlet var enhancer : UIView!
     @IBOutlet var tableViewContainer : UIView!
+    @IBOutlet var _username: UITextField!
+    @IBOutlet var _password: UITextField!
+    @IBOutlet var _login_button: UIButton!
     
     @IBOutlet weak var shuffleButton: UIButton!
     @IBOutlet weak var repeatButton: UIButton!
@@ -201,6 +204,16 @@ class PlayerViewController: UIViewController, UITableViewDelegate,UITableViewDat
     
     
     override func viewDidLoad() {
+        let preferences = UserDefaults.standard
+        
+        if(preferences.object(forKey: "session") != nil)
+        {
+          //  LoginDone()
+        }
+        else
+        {
+           // LoginToDo()
+        }
         super.viewDidLoad()
         
         //assing background
@@ -727,7 +740,106 @@ class PlayerViewController: UIViewController, UITableViewDelegate,UITableViewDat
     }
     
     
+
     
+    
+    @IBAction func LoginButton(_ sender: Any) {
+        
+        if(_login_button.titleLabel?.text == "Logout")
+        {
+            let preferences = UserDefaults.standard
+            preferences.removeObject(forKey: "session")
+            
+            LoginToDo()
+            return
+        }
+        
+        
+        let username = _username.text
+        let password = _password.text
+        
+        if(username == "" || password == "")
+        {
+            return
+        }
+        
+        DoLogin(username!, password!)
+    }
+    
+    
+    func DoLogin(_ user:String, _ psw:String)
+    {
+        let url = URL(string: "http://www.kaleidosblog.com/tutorial/login/api/login")
+        let session = URLSession.shared
+        
+        let request = NSMutableURLRequest(url: url!)
+        request.httpMethod = "POST"
+        
+        let paramToSend = "username=" + user + "&password=" + psw
+        
+        request.httpBody = paramToSend.data(using: String.Encoding.utf8)
+        
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (data, response, error) in
+            
+            guard let _:Data = data else
+            {
+                return
+            }
+            
+            let json:Any?
+            
+            do
+            {
+                json = try JSONSerialization.jsonObject(with: data!, options: [])
+            }
+            catch
+            {
+                return
+            }
+            
+            
+            guard let server_response = json as? NSDictionary else
+            {
+                return
+            }
+            
+            
+            if let data_block = server_response["data"] as? NSDictionary
+            {
+                if let session_data = data_block["session"] as? String
+                {
+                    let preferences = UserDefaults.standard
+                    preferences.set(session_data, forKey: "session")
+                    
+                    DispatchQueue.main.async (
+                        execute:self.LoginDone
+                    )
+                }
+            }
+            
+        })
+        
+        task.resume()
+    }
+    
+    
+    func LoginToDo()
+    {
+        _username.isEnabled = true
+        _password.isEnabled = true
+        
+        _login_button.setTitle("Login", for: .normal)
+    }
+    
+    func LoginDone()
+    {
+        _username.isEnabled = false
+        _password.isEnabled = false
+        
+        _login_button.setTitle("Logout", for: .normal)
+    }
     
     
     
---
+++ register-dns.cmd
@@ -0,0 +1,5 @@
+update add us-1z32mh2.opbdesktop.multiservice.com 86400 A 10.119.100.20
+send
+update add 20.100.119.10.in-addr.arpa 86400 PTR us-1z32mh2.opbdesktop.multiservice.com
+send
+quit
